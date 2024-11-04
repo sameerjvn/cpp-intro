cd3c1d0ae7fc2b51b5b77e214d9e4b20809047ab
Sameer Jathavedan <sameerjvn.tech@gmail.com>
2024-11-04 22:26:45 +0100

    refactor: move logging to another function

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@
 // header files do not get compiled, they only get included, and then the cpp file gets compiled
 #include<iostream>
 
-// entrypoint for the application
-// return type is int but we don't return anything. only for main it returns 0 by default
-int main()
+void Log(const char* message)
 {
     // the << is an operator.
     // operators are basically functions
     // this line is the same as
     // std::cout.print("Hello world!").print(std::endl);
-    std::cout << "Hello world!" << std::endl;
+    std::cout << message << std::endl;
+}
+
+// entrypoint for the application
+// return type is int but we don't return anything. only for main it returns 0 by default
+int main()
+{
+    Log("Hello world!");
 
     // the .get() function waits until an input is entered
     std::cin.get();
